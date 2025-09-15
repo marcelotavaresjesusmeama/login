3d9e6547a2326188f320da6ed874817755101efa
marcelotavaresjesusmeama <marcelotavaresjesusmeama@hotmail.com>
2025-09-14 21:30:05 -0300

    segundo commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function logar(){
+    var login = document.getElementById("login").value;
+
+    if(login == "marcelo"){
+        alert('Logado com Sucesso');
+    }else{
+        alert("Usuário ou senha incorreto")
+    }
+}
